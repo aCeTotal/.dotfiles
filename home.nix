b0d7d4419aaa520f6709b8c9a86b7c799adef8ae
Lars <lars.oksendal@gmail.com>
2024-01-07 20:53:50 +0100

    configuration.nix

--- home.nix
+++ home.nix
@@ -6,24 +6,6 @@
       home.homeDirectory = "/home/total";
       home.stateVersion = "24.05";
 
-    # Hyprland - Tiling Window Manager Installation
-    wayland.windowManager.hyprland = {
-      # Whether to enable Hyprland wayland compositor
-      enable = true;
-  
-      # The hyprland package to use
-      package = pkgs.hyprland;
-
-      # Whether to enable XWayland
-      xwayland.enable = true;
-
-      # Whether to enable hyprland-session.target on hyprland startup
-      systemd.enable = true;
-
-      # Whether to enable patching wlroots for better Nvidia support
-      enableNvidiaPatches = true;
-    };
-
     home.packages = [
 
     ];
