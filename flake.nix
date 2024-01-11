4e6f64e003770e7065fa60b70efcd5cc00684fd8
lars <lars.oksendal@gmail.com>
2024-01-11 01:09:00 +0100

    hm

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         nixosConfigurations = {
             desktop = lib.nixosSystem {
                 inherit system;
-		modules = [./hosts ];
+		modules = [./. "+" "/desktop/desktop.nix"];
             };
             
             laptop = lib.nixosSystem {
