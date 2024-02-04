fd5bab2309691699403edfca8cc4dca2c0c16951
lars <lars.oksendal@gmail.com>
2024-02-04 01:41:58 +0100

    xwayland debugging

--- system/desktop/configuration.nix
+++ system/desktop/configuration.nix
@@ -76,7 +76,7 @@
 	# If your cursor becomes invisible
 	WLR_NO_HARDWARE_CURSORS = "1";
 	# Hint electron apps to use wayland
-	NIXOS_OZONE_WL = "1";
+	NIXOS_OZONE_WL = "0";
   };
 
   # Power Management
