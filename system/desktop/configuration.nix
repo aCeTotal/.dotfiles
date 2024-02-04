66d630023771eadf419dc4492fe56e8884bf48fd
lars <lars.oksendal@gmail.com>
2024-02-04 01:02:12 +0100

    debugging xwayland

--- system/desktop/configuration.nix
+++ system/desktop/configuration.nix
@@ -25,27 +25,27 @@
 
   # Kernel setup
   boot.kernelPackages = pkgs.linuxPackages_zen;
-  boot.kernel.sysctl = {
-    "kernel.sysrq" = 1;                             # SysRQ for is rebooting their machine properly if it freezes: SOURCE: https://oglo.dev/tutorials/sysrq/index.html
-    "net.core.rmem_default" = 1073741824;           # Default socket receive buffer size, improve network performance & applications that use sockets
-    "net.core.rmem_max" = 1073741824;               # Maximum socket receive buffer size, determin the amount of data that can be buffered in memory for network operations
-    "net.core.wmem_default" = 1073741824;           # Default socket send buffer size, improve network performance & applications that use sockets
-    "net.core.wmem_max" = 1073741824;               # Maximum socket send buffer size, determin the amount of data that can be buffered in memory for network operations
-    "net.ipv4.tcp_rmem" = "4096 87380 1073741824";  # 1 GiB max
-    "net.ipv4.tcp_wmem" = "4096 87380 1073741824";  # 1 GiB max
-    "net.ipv4.tcp_keepalive_intvl" = 30;            # TCP keepalive interval between probes, TCP keepalive probes, which are used to detect if a connection is still alive.
-    "net.ipv4.tcp_keepalive_probes" = 5;            # TCP keepalive probes, TCP keepalive probes, which are used to detect if a connection is still alive.
-    "net.ipv4.tcp_keepalive_time" = 300;            # TCP keepalive interval (seconds), TCP keepalive probes, which are used to detect if a connection is still alive.
-    "vm.dirty_background_bytes" = 268435456;        # 256 MB in bytes, data that has been modified in memory and needs to be written to disk
-    "vm.dirty_bytes" = 1073741824;                  # 1 GB in bytes, data that has been modified in memory and needs to be written to disk
-    "vm.min_free_kbytes" = 65536;                   # Minimum free memory for safety (in KB), can help prevent memory exhaustion situations
-    "vm.swappiness" = 1;                            # how aggressively the kernel swaps data from RAM to disk. Lower values prioritize keeping data in RAM,
-    "vm.vfs_cache_pressure" = 50;                   # Adjust vfs_cache_pressure (0-1000), how the kernel reclaims memory used for caching filesystem objects
-    "fs.aio-max-nr" = 1048576;                      # defines the maximum number of asynchronous I/O requests that can be in progress at a given time.
-    "kernel.pid_max" = 4194304;                     # allows a large number of processes and threads to be managed
-    "net.ipv4.tcp_congestion_control" = "bbr";
-    "net.core.default_qdisc" = "fq";
-  };
+  #boot.kernel.sysctl = {
+  #  "kernel.sysrq" = 1;                             # SysRQ for is rebooting their machine properly if it freezes: SOURCE: https://oglo.dev/tutorials/sysrq/index.html
+  #  "net.core.rmem_default" = 1073741824;           # Default socket receive buffer size, improve network performance & applications that use sockets
+  #  "net.core.rmem_max" = 1073741824;               # Maximum socket receive buffer size, determin the amount of data that can be buffered in memory for network operations
+  #  "net.core.wmem_default" = 1073741824;           # Default socket send buffer size, improve network performance & applications that use sockets
+  #  "net.core.wmem_max" = 1073741824;               # Maximum socket send buffer size, determin the amount of data that can be buffered in memory for network operations
+  #  "net.ipv4.tcp_rmem" = "4096 87380 1073741824";  # 1 GiB max
+  #  "net.ipv4.tcp_wmem" = "4096 87380 1073741824";  # 1 GiB max
+  #  "net.ipv4.tcp_keepalive_intvl" = 30;            # TCP keepalive interval between probes, TCP keepalive probes, which are used to detect if a connection is still alive.
+  #  "net.ipv4.tcp_keepalive_probes" = 5;            # TCP keepalive probes, TCP keepalive probes, which are used to detect if a connection is still alive.
+  #  "net.ipv4.tcp_keepalive_time" = 300;            # TCP keepalive interval (seconds), TCP keepalive probes, which are used to detect if a connection is still alive.
+  #  "vm.dirty_background_bytes" = 268435456;        # 256 MB in bytes, data that has been modified in memory and needs to be written to disk
+  #  "vm.dirty_bytes" = 1073741824;                  # 1 GB in bytes, data that has been modified in memory and needs to be written to disk
+  #  "vm.min_free_kbytes" = 65536;                   # Minimum free memory for safety (in KB), can help prevent memory exhaustion situations
+  #  "vm.swappiness" = 1;                            # how aggressively the kernel swaps data from RAM to disk. Lower values prioritize keeping data in RAM,
+  #  "vm.vfs_cache_pressure" = 50;                   # Adjust vfs_cache_pressure (0-1000), how the kernel reclaims memory used for caching filesystem objects
+  #  "fs.aio-max-nr" = 1048576;                      # defines the maximum number of asynchronous I/O requests that can be in progress at a given time.
+  #  "kernel.pid_max" = 4194304;                     # allows a large number of processes and threads to be managed
+  #  "net.ipv4.tcp_congestion_control" = "bbr";
+  #  "net.core.default_qdisc" = "fq";
+ # };
 
   # Extra BOOT settings
   boot.supportedFilesystems = [ "btrfs" "ntfs" ];
@@ -53,10 +53,6 @@
   boot.tmp.cleanOnBoot = true;
   boot.modprobeConfig.enable = true;
   boot.extraModprobeConfig = ''
-	  options nvidia NVreg_RegistryDwords="OverrideMaxPerf=0x1
-	  nvidia.NVreg_RegistryDwords=EnableBrightnessControl=1
-	  options nvidia NVreg_UsePageAttributeTable=1
-	  option nvidia NVreg_RegistryDwords="PowerMizerEnable=0x1; PerfLevelSrc=0x3322; PowerMizerDefaultAC=0x1
 ''; 
   hardware.enableAllFirmware = true;
   nixpkgs.config.allowUnfree = true;
