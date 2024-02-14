c50339fec67c1de5a08b055e23467f963ab53763
lars <lars.oksendal@gmail.com>
2024-02-14 23:58:10 +0100

    inputs

--- flake.nix
+++ flake.nix
@@ -1,16 +1,15 @@
 {
-    description = "aCeTotal's FLAKE";
+    description = "TotalOS";
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
         home-manager.url = "github:nix-community/home-manager/master";
         home-manager.inputs.nixpkgs.follows = "nixpkgs";
-        hyprland.url = "github:hyprwm/Hyprland";
-
-        nixvim = {
-          url = "github:nix-community/nixvim";
-          inputs.nixpkgs.follows = "nixpkgs";
-        };
+	hyprland.url = "github:hyprwm/Hyprland";
+    	hyprland-plugins = {
+      		url = "github:hyprwm/hyprland-plugins";
+      		inputs.hyprland.follows = "hyprland";
+    	};
     };
 
     outputs = inputs@{ self, nixpkgs, home-manager, ... }:
@@ -21,16 +20,23 @@
         in  {
         nixosConfigurations = {
             desktop = lib.nixosSystem {
-              specialArgs = {inherit inputs; inherit system;}
+              specialArgs = {inherit inputs; inherit system;};
                 modules = [ ./system/desktop/configuration.nix 
-			        home-manager.nixosModules.home-manager {
-	    	            home-manager.useGlobalPkgs = true;
-            	        home-manager.useUserPackages = true;
-            	        home-manager.backupFileExtension = "backup";
-	    		        home-manager.users.total = import ./system/desktop/home.nix;
-	    	        }
-		        ];
+			home-manager.nixosModules.home-manager {
+	    			home-manager.useGlobalPkgs = true;
+            			home-manager.useUserPackages = true;
+            			home-manager.backupFileExtension = "backup";
+	    			home-manager.users.total = import ./system/desktop/home.nix;
+	    		}
+		];
             };
-   	    };
+
+
+
+
+   	 };
+
       };
+
 }
+
