22d81d57907126a805fd53b1ba575e74f0e892af
lars <lars.oksendal@gmail.com>
2024-03-08 15:30:40 +0100

    x11 vm config

--- hosts/x11vm/modules/bash.nix
+++ hosts/x11vm/modules/bash.nix
@@ -8,13 +8,13 @@
         ll = "ls -l";
         ".." = "cd ..";
 	"v" = "nvim";
-	"update" = "cd $HOME/.dotfiles && sudo nixos-rebuild switch --flake .#desktop";
-	"upgrade" = "cd $HOME/.dotfiles && nix flake update && sudo nixos-rebuild switch --flake .#desktop";
+	"update" = "cd $HOME/.dotfiles && sudo nixos-rebuild switch --flake .#x11vm";
+	"upgrade" = "cd $HOME/.dotfiles && nix flake update && sudo nixos-rebuild switch --flake .#x11vm";
 
 
-	"editconf" = "nvim ~/.dotfiles/hosts/desktop/configuration.nix";
-	"edithome" = "nvim ~/.dotfiles/hosts/desktop/home.nix";
-	"modules" = "cd ~/.dotfiles/hosts/desktop/modules";
+	"editconf" = "nvim ~/.dotfiles/hosts/x11vm/configuration.nix";
+	"edithome" = "nvim ~/.dotfiles/hosts/x11vm/home.nix";
+	"modules" = "cd ~/.dotfiles/hosts/x11vm/modules";
       };        
     };
 
