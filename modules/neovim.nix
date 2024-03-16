8269d7f8b72d782d499f318b8b1c0724360278e5
lars <lars.oksendal@gmail.com>
2024-03-16 13:53:52 +0100

    neovim

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,125 +1,54 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-    home.file.".config/nvim/init.lua".text = ''
-local o = vim.opt
-local g = vim.g
-
--- Autocmds
-vim.cmd [[
-augroup CursorLine
-    au!
-    au VimEnter * setlocal cursorline
-    au WinEnter * setlocal cursorline
-    au BufWinEnter * setlocal cursorline
-    au WinLeave * setlocal nocursorline
-augroup END
-
-autocmd FileType nix setlocal shiftwidth=2
-
-
-]]
-
--- Keybinds
-local map = vim.api.nvim_set_keymap
-local opts = { silent = true, noremap = true }
-
-map("n", "<C-h>", "<C-w>h", opts)
-map("n", "<C-j>", "<C-w>j", opts)
-map("n", "<C-k>", "<C-w>k", opts)
-map("n", "<C-l>", "<C-w>l", opts)
-map('n', '<C-n>', ':Telescope live_grep <CR>', opts)
-map('n', '<C-f>', ':Telescope find_files <CR>', opts)
-map('n', 'j', 'gj', opts)
-map('n', 'k', 'gk', opts)
-map('n', ';', ':', { noremap = true } )
-
-g.mapleader = ' '
-
--- Performance
-o.lazyredraw = true;
-o.shell = "bash"
-o.shadafile = "NONE"
-
--- Colors
-o.termguicolors = true
-
--- Undo files
-o.undofile = true
-
--- Indentation
-o.smartindent = true
-o.tabstop = 2
-o.shiftwidth = 2
-o.shiftround = true
-o.expandtab = true
-o.scrolloff = 3
-
--- Set clipboard to use system clipboard
-o.clipboard = "unnamedplus"
-
--- Use mouse
-o.mouse = "a"
-
--- Nicer UI settings
-o.cursorline = true
-o.relativenumber = true
-o.number = true
-
--- Get rid of annoying viminfo file
-o.viminfo = ""
-o.viminfofile = "NONE"
-
--- Miscellaneous quality of life
-o.ignorecase = true
-o.ttimeoutlen = 5
-o.hidden = true
-o.shortmess = "atI"
-o.wrap = false
-o.backup = false
-o.writebackup = false
-o.errorbells = false
-o.swapfile = false
-o.showmode = false
-o.laststatus = 3
-o.pumheight = 6
-o.splitright = true
-o.splitbelow = true
-o.completeopt = "menuone,noselect"
-      
-    '';
-
-  home.file.".config/nvim/treesitter.lua".text = ''
-  require'nvim-treesitter.configs'.setup {
-    ensure_installed = "maintained",
-    ignore_install = { "" },
-    highlight = {
-    enable = true,
-    disable = { },
-    },
-  }
-
-  '';
-
 
   programs.neovim = {
   	enable = true;
-	  viAlias = true;
-	  vimAlias = true;
-    vimdiffAlias = true;
-  	extraConfig = ''
-      luafile ~/.dotfiles/nvim/settings.lua
-      luafile ~/.dotfiles/nvim/treesitter.lua
-  	'';
+	viAlias = true;
+	vimAlias = true;
+        vimdiffAlias = true;
+        extraConfig = builtins.concatStringsSep "\n" [
+          ''
+          luafile ${builtins.toString ~/.dotfiles/nvim/settings.lua}
+          luafile ${builtins.toString ~/.dotfiles/nvim/line.lua}
+          luafile ${builtins.toString ~/.dotfiles/nvim/lsp.lua}
+          luafile ${builtins.toString ~/.dotfiles/nvim/ide.lua}
+          luafile ${builtins.toString ~/.dotfiles/nvim/tabs.lua}
+          luafile ${builtins.toString ~/.dotfiles/nvim/tree.lua}
+          ''
+        ];
 
-    plugins = with pkgs.vimPlugins; [
-        # Fiel tree
-        nvim-web-devicons
-        nvim-tree-lua
-
-        # Languages
-        vim-nix
+      plugins = with pkgs.vimPlugins; [
+        # Base
+        popup-nvim
+        plenary-nvim
         nvim-treesitter
-  
+        telescope-nvim
+        vim-fugitive
+        vim-markdown
+        vim-trailing-whitespace
+        vim-surround
+        vim-which-key
+        # Tabs
+        barbar-nvim
+        # EyeCandy
+        wal-vim
+        nvim-colorizer-lua
+        nvim-cursorline
+        nvim-web-devicons
+        # Status Line
+        lualine-nvim   
+        # File tree
+        nvim-tree-lua
+        # Language
+        vim-nix
+        # LSP
+        cmp-buffer
+        cmp-nvim-lsp
+        nvim-cmp
+        nvim-compe
+        nvim-lspconfig
+    ];  
 
+  };
 }
