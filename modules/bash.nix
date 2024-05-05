399088b2af32609d76671193ae5688894fbd19f7
aCeTotal <lars.oksendal@gmail.com>
2024-05-05 13:54:11 +0200

    gs66

--- modules/bash.nix
+++ modules/bash.nix
@@ -14,6 +14,7 @@
         "ls" = "ls -l --color=auto";
         ".." = "cd ..";
 	"v" = "nvim";
+	"drone" = "cd /mnt/nfs/Bigdisk1/dev/stm32/drone/";
 	"dev" = "cd /mnt/nfs/Bigdisk1/dev/imgui/src && shell";
 	"build" = "cd /mnt/nfs/Bigdisk1/dev/imgui/src/build/ && cmake .. -G Ninja && ninja";
 	"demo" = "cd /mnt/nfs/Bigdisk1/dev/imgui/src/bin/Linux64/ && ./securitydrone";
