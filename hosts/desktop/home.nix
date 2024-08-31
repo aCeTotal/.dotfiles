1236fe11b92d6b16fc03cab9f11b10b126ba4fb7
aCeTotal <lars.oksendal@gmail.com>
2024-08-31 22:10:06 +0200

    derivations

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -16,7 +16,7 @@
       ../../modules/user/hyprland.nix
       ../../modules/user/waybar.nix
       ../../modules/user/virtualisation.nix
-    #  ../../modules/user/derivations.nix
+      ../../modules/user/derivations.nix
     #  ../../modules/user/tmux.nix
     ];
 
