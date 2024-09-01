63d62f913dfb905e5c6dd5dcf0440712be715c85
aCeTotal <lars.oksendal@gmail.com>
2024-09-01 16:37:07 +0200

    derivations

--- modules/user/derivations.nix
+++ modules/user/derivations.nix
@@ -1,14 +1,12 @@
 { pkgs, ... }:
 
 let
-  chitubox = import ../derivations/chitubox/default.nix { inherit pkgs; };
   pureref = import ../derivations/pureref/default.nix { inherit pkgs; };
 
 in
 {
 
     home.packages = [
-    chitubox
     pureref
     ];
 }
