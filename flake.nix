f6426e11a3549cc312716758f99764e613df1c1d
aCeTotal <lars.oksendal@gmail.com>
2025-02-17 22:45:07 +0100

    homeserver

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-homeserver.url = "github:nixos/nixpkgs/0da3c44a9460a26d2025ec3ed2ec60a895eb1114"; # Nixpkgs 2024
+    nixpkgs-homeserver.url = "github:nixos/nixpkgs/43e0d3ff4bee6245c977164f909ff5d6303e8028"; # Nixpkgs Jan, 2025
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager.url = "github:nix-community/home-manager/master";
   };
