cec460be94f7bca4b78b17ab86c6f67922f84b66
aCeTotal <lars.oksendal@gmail.com>
2025-02-18 20:05:59 +0100

    homeserver

--- hosts/homeserver/configuration.nix
+++ hosts/homeserver/configuration.nix
@@ -48,7 +48,6 @@ services.mysql = {
   ensureUsers = [
     {
       name = "nixos";
-      password = "nixos";  # Legger til passord her
       ensurePermissions = {
         "pfo_db.*" = "ALL PRIVILEGES";
       };
