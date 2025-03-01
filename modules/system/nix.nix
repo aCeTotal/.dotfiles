8fcf9d117f5b0533ee66429bd1635bd6e6727a5f
aCeTotal <lars.oksendal@gmail.com>
2025-03-01 04:25:12 +0100

    nix

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -9,17 +9,24 @@
       options = "--delete-older-than 10d";
     };
     settings = {
-      max-jobs = 40;
-      download-buffer-size = 80000000;
+      max-jobs = 25;
+      download-buffer-size = 80000000000000;
+      sandbox = true;
+      extra-sandbox-paths = [ "/etc" "/var" "/home" ];
+      keep-going = true;
+      builders-use-substitutes = true;
       experimental-features = [ "nix-command" "flakes" ];
       auto-optimise-store = true;
-      substituters = [
-          "https://hyprland.cachix.org"
-      ];
-      trusted-public-keys = [
-          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      ];
     };
+
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://hyprland.cachix.org"
+    ];
+
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
   };
 
 }
