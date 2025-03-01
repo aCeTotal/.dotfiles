96ccd4b5d3488adc30959752bc71ae10a1cc2faa
aCeTotal <lars.oksendal@gmail.com>
2025-03-01 04:26:45 +0100

    nix

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -17,16 +17,15 @@
       builders-use-substitutes = true;
       experimental-features = [ "nix-command" "flakes" ];
       auto-optimise-store = true;
+
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://hyprland.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
     };
-
-    substituters = [
-      "https://cache.nixos.org/"
-      "https://hyprland.cachix.org"
-    ];
-
-    trusted-public-keys = [
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-    ];
   };
-
 }
