87f987633226baf43bfe4ea6bf927d3bb4a8af29
aCeTotal <lars.oksendal@gmail.com>
2025-05-09 18:56:42 +0200

    typo speedtree

--- modules/user/derivations.nix
+++ modules/user/derivations.nix
@@ -4,7 +4,7 @@ let
     #pureref = import ../derivations/pureref/default.nix { inherit pkgs; };
     uvtools = import ../derivations/uvtools/default.nix { inherit pkgs; };
     #o3de = import ../derivations/o3de/default.nix { inherit pkgs; };
-    speedtree = import ../derivations/speedtre/default.nix { inherit pkgs; };
+    speedtree = import ../derivations/speedtree/default.nix { inherit pkgs; };
 
 
 in
